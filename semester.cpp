1739b4940e2ad1ea23bfeaa6ea1880cadee1f522
Ganga Sagar Joshi <197456338+gangasagar12@users.noreply.github.com>
2025-10-27 21:58:52 +0545

    updated on the core function of the projects.

--- semester.cpp
+++ semester.cpp
@@ -38,7 +38,49 @@ void dashboard(int uid){
         cout<<" enter  your choice: ";
         cin >> choice;
         switch(choice){
+            case 1: create a post(uid):
+            break;
+            case 2: view posts(uid);
+            break;
+            case 3: like/comment();
+            break;
+            case 4: follow/ unfollow();
+            break;
+            default: cout<<" invalid choice";
+            break;
+
             
+        }
+    } while (choice != 0);
+    
+}
+//  body part of the main function
+int main(){
+    int choice;
+    do{
+        cout<<" \n ===== welcome to social media projects ====\n";
+        cout<<" \n 1. register\n";
+        cout<<" \n 2. login \n";
+        cout<<" \n 3. exit \n";
+        cout<<" enter your choice : ";
+        cin >> choice;
+        if(choice==1){
+            registeruser();
+
+        }
+        else if (choice==2){
+            int uid== Userlogin();
+            if(uid=-1){
+                dashboard(uid);
+                
+            }
+            else if (choice==3){
+                savedata();
+                cout<<" exiting the program tra again ans again.\n";
+                exit(0);
+            }
+            while(choice!=3);
+            return 0;
         }
     }
 }
