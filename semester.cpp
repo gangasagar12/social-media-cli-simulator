c74e4b8e00330b0b86a8e0cad871d6d129c82f1d
Ganga Sagar Joshi <197456338+gangasagar12@users.noreply.github.com>
2025-11-25 13:05:42 +0545

    recreating the  function used oop concepts

--- semester.cpp
+++ semester.cpp
@@ -189,10 +189,12 @@ class user: public person{
     vector<string> following;
 public:
     user(){}
-    user(const string &uname, const string &pass, const string &b)
-    : person(uname,b), password(pass) {}
-    ~user(){}
-
+    user(const string &uname, const string &pass, const string &b)  //
+  {
+    username=uname;
+    password=pass;
+    bio=b;
+  }
     // getters
     bool checkpassword(const string &p) const {
         return password==p;
@@ -206,465 +208,190 @@ public:
     const vector<string>& getfollowing() const {
         return following;
     }
-};
 
-
-
-
-
-
-vector<User> users;
-vector<Post>posts;
-int postCounter=1;
-// utility functions to split by delimiter string
-vector<string> split(const string &src , const string &delimiter){
-    vector<string> out;
-    string s =src;
-    size_t pos=0;
-     while((pos=s.find(delimiter))!=string::npos){
-        out.push_back(s.substr(0,pos));
-        s.erase(0, pos+ delimiter.length());
-     }
-     out.push_back(s);
-     return out;
-}
-// function that research user by username
-int finduser(const string &uname){
-    for ( int i=0;i<(int)users.size();i++){
-        if(users[i].username==uname)
-        return i;
-
+    ~user(){}  // destructor
+    // operations
+    void addpost(int pid){
+        posts.push_back(pid);
     }
-    return -1;
-}
-// function to save the all data into the file system
-void savedata(){
-  // users :
-  ofstream f("users.txt");
-if(!f){
-    cerr<< " error opening users.txt for writing: \n";
-    return;
-}
- for (auto &u : users){
-    f<<u.username<<"|"
-     <<u.password<<"|"
-     <<u.bio<<"|";
-
-     // save user's post ids
-     for ( int pid: u.posts){
-        f<<pid<<",";
-     }
-
-     f<<"|";
-     // save followers
-     for ( const string &fol: u.followers){
-        f<<fol<<",";
-     }
-
-     f<<"|";
-     // save following
-     for ( const string &fol: u.following){
-        f<<fol<<",";
-     }
-     f<<"\n";
- }
- f.close();
- // posts 
- ofstream p("posts.txt");
-    if(!p){
-        cerr<<" error opening posts.txt for writing \n";
-        return ;
+    void addfollower(const string &fol){
+        followers.push_back(fol);
     }
-    for (auto &po : posts){
-        p<<po.id<<"|"
-         <<po.author<<"|"
-         <<po.content<<"|"
-         <<po.likes<<"|";
-        // save comments separated by "~~"
-        for ( const string &c: po.comments)
-            p<<c<<"~~";
-        p<<"\n";
+    void addfollowing(const string &fol){
+        following.push_back(fol);
     }
-    p.close();
-}
-// load all data fron the files
+    void removepost(int pid){
+        posts.erase(remove(posts.begin(),posts.end(),pid),posts.end());
+    }
+    void removefollower(const string &fol){
+        followers.erase(remove(followers.begin(),followers.end(),fol),followers.end());
+    }
+    void removefollowing(const string &fol){
+        following.erase(remove(following.begin(),following.end(),fol),following.end());
+    }
+    void setbio(const string &b){
+        bio=b;
+    }
+    void showprofile() override {
+        cout<<" username: "<< username <<"\n";
+        cout<<" bio: "<< bio <<"\n";
+        cout<<" followers: "<< followers.size() << " | following: "<< following.size() <<"\n";  
+        cout<<" posts: \n";
+        if (posts.empty()){
+            cout<<" no posts available. \n";
+            return ;
+        }
+    }
+    //  serialization
+    string serialize() const {
+        
+        string out = username + "|" + password + "|" + bio + "|";
+        for (int pid : posts)
+         out += to_string(pid) + ",";
+        out += "|";
+        for (const string &fol : followers)
+         out += fol + ",";
+        out += "|";
+        for (const string &fol : following) 
+        out += fol + ",";
+        return out;
+    }
 
-void loadData() {
-    users.clear();
-    posts.clear();
-    postCounter = 1;
-
-    ifstream f("users.txt");
-    if (f) {
-        string line;
-        while (getline(f, line)) {
-            if (line.empty()) continue;
-            vector<string> parts = split(line, "|");
-            // Expect 6 fields: username, password, bio, posts, followers, following
-            while (parts.size() < 6) parts.push_back("");
-            User u;
+    static user parse( const string &line){
+        vector<string> parts = split(line, "|");
+        while (parts.size() < 6)
+            parts.push_back("");
+            user u;
             u.username = parts[0];
             u.password = parts[1];
             u.bio      = parts[2];
 
             // posts
-            if (!parts[3].empty()) {
-                vector<string> pids = split(parts[3], ",");
-                for (auto &x : pids)
-                    if (!x.empty())
-                        u.posts.push_back(stoi(x));
+            if(!parts[3].empty()){
+                vector<string> pids = split(parts[3],",");
+                for ( const auto &x: pids)
+                 if(!x.empty())
+                  u.posts.push_back(stoi(x));
             }
-
             // followers
-            if (!parts[4].empty()) {
-                vector<string> fol = split(parts[4], ",");
-                for (auto &x : fol)
-                    if (!x.empty())
-                        u.followers.push_back(x);
+            if(!parts[4].empty()){
+
             }
-
-            // following
-            if (!parts[5].empty()) {
-                vector<string> fol = split(parts[5], ",");
-                for (auto &x : fol)
-                    if (!x.empty())
-                        u.following.push_back(x);
-            }
-
-            users.push_back(u);
-        }
-        f.close();
-    }
-
-    ifstream p("posts.txt");
-    if (p) {
-        string line;
-        while (getline(p, line)) {
-            if (line.empty()) continue;
-            vector<string> parts = split(line, "|");
-            // Expect 5 fields: id, author, content, likes, commentsCombined
-            while (parts.size() < 5) parts.push_back("");
-            Post po;
-            try {
-                po.id = stoi(parts[0]);
-            } catch (...) { continue; }
-            po.author  = parts[1];
-            po.content = parts[2];
-            try {
-                po.likes = stoi(parts[3]);
-            } catch (...) { po.likes = 0; }
-
-            // comments separated by "~~"
-            if (!parts[4].empty()) {
-                vector<string> cms = split(parts[4], "~~");
-                for (auto &c : cms)
-                    if (!c.empty())
-                        po.comments.push_back(c);
-            }
-
-            posts.push_back(po);
-            if (po.id >= postCounter) postCounter = po.id + 1;
-        }
-        p.close();
-    }
-}
-// function to password masking  using getch function
-string maskpassword(){
-    string pass;
-    char ch;  // to store each character
-    while(true){
-        ch=getch(); // get charcater without echoing
-        if(ch==13){
-            cout<<"\n";
-            break;
-        }
-        else if(ch==8){
-            if(!pass.empty()){
-                pass.pop_back();
-                cout<<"\b \b"; // erase last char from console
-            }
-        }
-        else if (ch==0 || ch==224){
-            getch(); // ignore special keys
-        }
-        else{
-            pass.push_back(ch);
-            cout<<"*"; // print asterisk for each char
-        }
-    }
-    return pass;
-}
-
-//  main function  
-void registerUser() {
-    User u;
-    cout << "Enter username: ";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flush newline leftover
-    getline(cin, u.username);
-    if (u.username.empty()) {
-        cout << "Username cannot be empty.\n";
-        return;
-    }
-    if (finduser(u.username) != -1) {
-        cout << "Username already exists!\n";
-        return;
-    }
-
-    cout << "Enter password: ";
-    u.password = maskpassword();
-
-    cout << "Enter bio: ";
-    getline(cin, u.bio);
-
-    users.push_back(u);
-    savedata(); // persist immediately
-    cout << "Registration successful!\n";
-}
-// for the user login
-int userlogin(){
-    string name,pass;
-    cout<<"enter your name: ";
-    cin.clear(); // clear any previous error flags
-    cin>>name;
-    cout<<" enter  your password;";
-    cin>>pass;
- 
-    pass=maskpassword();  // get masked pasword
-    // search all users for username and password match
-    for ( int i=0;i<users.size();i++){
-        if(users[i].username==name && users[i].password==pass){
-            cout<<" login successful. \n";
-            return i;
-        }
-    }
-    cout<<" invalid username or password. please try again.\n";
-    return -1;
-}
-//  function of the ceate post 
-void createpost( int uid ){
-    Post p;
-    p.id=postCounter++;
-    p.author=users[uid].username;
-    cin.clear(); // clear input buffer
-    cin.ignore(); // flush newline leftover
-    cout<<" enter the content of your post: ";
-    getline(cin,p.content);
-    p.likes=0;
-    if (p.content.empty()){
-        cout<<" post content cannot be empty. \n";
-        return ;
-    }
-    posts.push_back(p);
-    users[uid].posts.push_back(p.id);
-    savedata(); // for the saving of data
-    cout<<" post created sucessfully.\n";
-    cout<<" post id: "<<p.id<<"\n";
-
-
-
-}
-
-// function  for the view posts of the account
-void viewposts(){
-    cout<<" \n News feed: \n";
-    if(posts.empty()){
-        cout<<" no posts available. \n";
-        return ;
-    }
-    for(auto it =posts.rbegin(); it!=posts.rend(); ++it){
-        cout<<" Post ID: "<<it-> id<<" by "<< it-> author<<"\n";
-        cout<< it-> content <<"\n";
-        cout<<" Likes: "<< it-> likes << " | Comments: "<< it-> comments.size()
-        <<"\n-----------------------\n";
-        if(!it->comments.empty()){
-            cout<<" Comments: \n";
-            for ( const string &c: it-> comments){
-                cout<<" - "<< c <<"\n";
-            }
-        }
-    }
-}
-// function for the like and commant of the posst
-void likeComment() {
-    cout << "Enter Post ID: ";
-    int pid;
-    if (!(cin >> pid)) {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input.\n";
-        return;
-    }
-    for (auto &p : posts) {
-        if (p.id == pid) {
-            int ch;
-            cout << "1. Like\n";
-            cout << "2. Comment\n";
-            cout << " Enter choice: ";
-            cin >> ch;
-            if (! ch) {
-                 cin.clear();
-                  cin.ignore();
-                   cout << "Invalid.\n"; 
-                   return; 
-                }
-            if (ch == 1) {
-                p.likes++;
-                savedata();
-                cout << "Liked!\n";
-            } else if (ch == 2) {
-                cin.ignore(); // clear input buffer   
-                string c;
-                cout << "Enter comment: ";
-                getline(cin, c);
-                if (!c.empty()) {
-                    p.comments.push_back(c);
-                    savedata();
-                    cout << "Comment added!\n";
-                } else {
-                    cout << "Comment empty. Aborted.\n";
-                }
-            } else {
-                cout << "Invalid choice.\n";
-            }
-            return;
-        }
-    }
-    cout << "Post not found!\n";
-}
-
-void followunfollow(int uid){
-    string uname;
-    cout<<" enter the userame to follow / unfollow: ";
-    cin>>uname;
-    cin.clear();
-    cin.ignore(); // clear input buffer  
-    if(uname.empty()){
-        cout<<" invalid username. \n";
-        return ;
-    }
-    int idx= finduser(uname);
-    if (idx==-1){
-        cout<<" user not found: \n";
-        return ;
+            return u;
 
     }
-    if(uid==idx){
-        cout<<" you cannot follow / unfollow yourself. \n";
-        return ;
-    }
-    auto &flist=users[uid].following;
-    auto &targetfollowers= users[idx].followers;
-    auto it = find(targetfollowers.begin(), targetfollowers.end(), users[uid].username);
-    if(find(flist.begin(),flist.end(),uname)!=flist.end()){
-        flist.erase(remove(flist.begin(),flist.end(),uname),flist.end());
-        cout<< " you  have been  unfollowed: "<<uname<<"\n";
-
-    }
-    else{
-        flist.push_back(uname);
-         targetfollowers.push_back(users[uid].username);
-         savedata();
-        cout<<" you have been followed : "<<uname<<"\n";
-
-    }
-}
-//  view profile 
-void viewprofile(int uid){
-    cout<<" PROFILE : ";
-    cout<<" username: "<<users[uid].username<<"\n";
-    cout<<" Bio : "<<users[uid].bio<<"\n";
-    cout<<" Followers: "<< users[uid].followers.size()<<" | Following: "<< users[uid].following.size()<<"\n";
-    cout<< " posts: \n";
-    if (users[uid].posts.empty()){
-        cout<<" no posts available. \n";
-        return ;
-    }
-    for (int pid: users[uid].posts){
-        for(auto &p : posts){
-            if(p.id==pid){
-                cout<<" "<<p.id<<": "<<p.content<<"\n";
-                
-            }
-        }
-    }
-}
-
-// function for the search user 
-void searchuser(){
-    string key;
-
-    cout<<" enter the username to serach: ";
-    cin>>key;
-    cout<<" mathching users: \n";
-    if(key.empty()){
-        cout<<" invalid username. \n";
-        return ;
-    }
-    for( auto &u: users){
-        if(u.username.find(key)!=string::npos){
-            cout<<" -"<<u.username<<"\n";
-
-        }
-    }
-}
-// dashboard 
-void dashboard(int uid){
-    int choice;
-    do{
-        cout<<" \n====== DASHBOARD=====\n";
-        cout<<"\n1. create post\n";
-        cout<<"\n2. view posts\n";
-        cout<<"\n3.like/comment\n";
-        cout<<"\n4. follow/ unfollow\n";
-        cout<<" enter  your choice: ";
-        cin >> choice;
-        switch(choice){
-            case 1: createpost(uid);
-            break;
-            case 2: viewposts();
-            break;
-            case 3: likeComment();
-            break;
-            case 4: followunfollow(uid);
-            break;
-            default: cout<<" invalid choice";
-            break;
-
-            
-        }
-    } while (choice != 0);
     
-}
-//  body part of the main function
-int main(){
-    loadData();
-    int choice;
-    do {
-        cout<<" \n ===== welcome to social media projects ====\n";
-        cout<<" \n 1. register\n";
-        cout<<" \n 2. login \n";
-        cout<<" \n 3. exit \n";
-        cout<<" enter your choice : ";
-        cin >> choice;
-        if(choice==1){
-            registerUser();
+private:
+     static string cape(const string &s) {
+        string out;
+        for (char ch : s) {
+            if (ch == '|') out += "&#124;";
+            else out += ch;
+        }
+        return out;
+    }
+    static string scapeTilde(const string &s) {
+        string out;
+        for (char ch : s) {
+            if (ch == '~') out += "&#126;";
+            else out += ch;
+        }
+        return out;
+    }
+class app{
+    private:
+        vector<user>users;
+        vector<Post>posts;
+        int postcounter=1;
+    
+        const string USERS_FILE="users.txt";
+        const string POSTS_FILE="posts.txt";
+        public:
+        app(){
+            loaddata();
+        }
+        ~app(){
+            saveall();
+        }
 
+        void saveall(){
+            vector<string>ulines;
+            for ( const auto &u: users){
+                ulines.push_back(u.serialize());
+            }
+            FileHandler::saveLines(USERS_FILE, ulines);
+            //  save posts
+            vector<string>plines;
+            for ( const auto &p: posts){
+                plines.push_back(p.serialize());
+            }
+            FileHandler::saveLines(POSTS_FILE, plines);
         }
-        else if (choice==2){
-            int uid= userlogin();
-            if(uid!=-1){
-                dashboard(uid);
-                
+        //  load data
+        void loadall(){
+            users.clear();   // clear existing data
+            posts.clear();  // clear existing data
+            postcounter=1;  // reset post counter
+            vector<string>ulines= FileHandler::loadLines(USERS_FILE);
+            for(auto &ln: ulines){
+                user u= user::parse(ln);
+                users.push_back(u);
+            
             }
-            else if (choice==3){
-                savedata();
-                cout<<" exiting the program tra again ans again.\n";
-                exit(0);
-            }
-            else{
-                cout<<" invilid choice please try againn.\n";
+            // loads posts
+            vector<string>plines= FileHandler::loadLines(POSTS_FILE);
+            for(const auto &ln: plines){
+                Post p= Post::parse(ln);
+                posts.push_back(p);
+                if (p.getid()>= postcounter)
+                postcounter=p.getid()+1;
             }
         }
-    } while (choice !=3);
-    return 0;
-}
+        // function for register user
+    void registeruser(){
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string uname;
+        cout<<" enter username: ";
+        getline(cin, uname);
+        if(uname.empty()){
+            cout<<" username cannot be empty. \n";
+            return ;
+        }
+        if(finduser(uname)!=-1){
+            cout<<" username already exists! \n";
+            return ;
+        }
+        cout<<" enter password: ";
+        string pass= maskpassword(); // password masking
+        cout<<" enter bio: ";
+        string bio;
+        getline(cin, bio);
+        users.emplace_back(uname, pass, bio);
+        saveall();
+        cout<<" registration successful! \n";
+
+    }
+    int loginuser(){
+        string uname;
+        cout<<" enter username: ";
+        cin>>uname;
+        cout<<" enter password: ";
+        string pass= maskpassword();
+        int idx=finduser(uname);
+        if(idx==-1){
+            cout<<" username not found. \n";
+            return -1;
+
+    }
+        if(!users[idx].checkpassword(pass)){
+            cout<<" wrong password. \n";
+            return -1;
+        }
+        cout<<" login successful. welcome, "<< uname <<"! \n";
+        return idx;
+    }
+};
+
+// for the creating the password
+
+
